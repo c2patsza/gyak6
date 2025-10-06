e3144400aed43f265ab408c5d338586bfd650674
c2ordben <c2ordben@vasvari.org>
2025-10-06 09:39:47 +0200

    developos valtoztatos feladat (#1)

--- aremeles.py
+++ aremeles.py
@@ -2,4 +2,4 @@ ar = int(input('Az alkatresz jelenlegi ara: '))
 
 aremeles = float(input('Aremeles (szazalekban): '))
 
-print(f'Az alkatresz uj ara: {ar * aremeles}')
+print(f'Az alkatresz uj ara: {ar * aremeles}') 
